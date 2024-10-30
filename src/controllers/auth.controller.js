7fba5cab7e07cf0f45dc3a434d213ef67e8f8e40
Anish <anishtanvar@gmail.com>
2024-10-30 16:03:34 +0530

    Update cookie options in login controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -76,6 +76,7 @@ const loginUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
+    sameSite: "None"
   };
 
   return res
